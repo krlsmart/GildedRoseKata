394f9f953ce483df0e0f6386140550fe44ccb837
xramentol <xramentol@voxelgroup.net>
2024-06-06 12:06:33 +0200

    chore: update fixture

--- GildedRoseTests/TextTestFixture.cs
+++ GildedRoseTests/TextTestFixture.cs
@@ -11,7 +11,7 @@ public static class TextTestFixture
         Console.WriteLine("OMGHAI!");
 
         var items = new List<Item>{
-            new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+            new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 30},
             new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
             new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
             new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
