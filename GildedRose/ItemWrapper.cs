2fd93412786807656dc1a7f37b4a846c66144dee
xramentol <xramentol@voxelgroup.net>
2024-06-06 12:12:54 +0200

    FEAT: extract wrapper class

---
+++ GildedRose/ItemWrapper.cs
@@ -0,0 +1,13 @@
+﻿namespace GildedRoseKata;
+
+public abstract class ItemWrapper
+{
+    protected readonly Item _item;
+
+    public ItemWrapper(Item item)
+    {
+        _item = item;
+    }
+
+    public abstract void UpdateQuality();
+}
