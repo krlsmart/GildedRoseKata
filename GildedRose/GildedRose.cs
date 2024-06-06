c500094ac43124610484f9708fccb31b420ae76d
Carlos Martinez <krlsmartnz@gmail.com>
2024-06-06 12:00:05 +0200

    refactor: lift up aged brie conditional

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -13,74 +13,95 @@ public class GildedRose
 
     public void UpdateQuality()
     {
-        for (var i = 0; i < _items.Count; i++)
+        for(var i = 0; i < _items.Count; i++)
         {
-            if (_items[i].Name != "Aged Brie" && _items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+            var isAgedBrie = _items[i].Name == "Aged Brie";
+
+            if(isAgedBrie)
             {
-                if (_items[i].Quality > 0)
+                if(_items[i].Quality < 50)
                 {
-                    if (_items[i].Name != "Sulfuras, Hand of Ragnaros")
+                    _items[i].Quality = _items[i].Quality + 1;
+                }
+
+                _items[i].SellIn = _items[i].SellIn - 1;
+
+                if(_items[i].SellIn < 0)
+                {
+                    if(_items[i].Quality < 50)
                     {
-                        _items[i].Quality = _items[i].Quality - 1;
+                        _items[i].Quality = _items[i].Quality + 1;
                     }
                 }
             }
             else
             {
-                if (_items[i].Quality < 50)
+                if(_items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    _items[i].Quality = _items[i].Quality + 1;
-
-                    if (_items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                    if(_items[i].Quality < 50)
                     {
-                        if (_items[i].SellIn < 11)
+                        _items[i].Quality = _items[i].Quality + 1;
+
+                        if(_items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
                         {
-                            if (_items[i].Quality < 50)
+                            if(_items[i].SellIn < 11)
                             {
-                                _items[i].Quality = _items[i].Quality + 1;
+                                if(_items[i].Quality < 50)
+                                {
+                                    _items[i].Quality = _items[i].Quality + 1;
+                                }
+                            }
+
+                            if(_items[i].SellIn < 6)
+                            {
+                                if(_items[i].Quality < 50)
+                                {
+                                    _items[i].Quality = _items[i].Quality + 1;
+                                }
                             }
                         }
-
-                        if (_items[i].SellIn < 6)
-                        {
-                            if (_items[i].Quality < 50)
-                            {
-                                _items[i].Quality = _items[i].Quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (_items[i].Name != "Sulfuras, Hand of Ragnaros")
-            {
-                _items[i].SellIn = _items[i].SellIn - 1;
-            }
-
-            if (_items[i].SellIn < 0)
-            {
-                if (_items[i].Name != "Aged Brie")
-                {
-                    if (_items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (_items[i].Quality > 0)
-                        {
-                            if (_items[i].Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                _items[i].Quality = _items[i].Quality - 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        _items[i].Quality = _items[i].Quality - _items[i].Quality;
                     }
                 }
                 else
                 {
-                    if (_items[i].Quality < 50)
+                    if(_items[i].Quality > 0)
                     {
-                        _items[i].Quality = _items[i].Quality + 1;
+                        if(_items[i].Name == "Sulfuras, Hand of Ragnaros")
+                        {
+                        }
+                        else
+                        {
+                            _items[i].Quality = _items[i].Quality - 1;
+                        }
+                    }
+                }
+
+                if(_items[i].Name == "Sulfuras, Hand of Ragnaros")
+                {
+                }
+                else
+                {
+                    _items[i].SellIn = _items[i].SellIn - 1;
+                }
+
+                if(_items[i].SellIn < 0)
+                {
+                    if(_items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        _items[i].Quality = _items[i].Quality - _items[i].Quality;
+                    }
+                    else
+                    {
+                        if(_items[i].Quality > 0)
+                        {
+                            if(_items[i].Name == "Sulfuras, Hand of Ragnaros")
+                            {
+                            }
+                            else
+                            {
+                                _items[i].Quality = _items[i].Quality - 1;
+                            }
+                        }
                     }
                 }
             }
