b3823c9e1fc6efbb6cb0080451a448cd015c659a
xramentol <xramentol@voxelgroup.net>
2024-06-06 12:18:24 +0200

    FEAT: create agedBrie

---
+++ GildedRose/AgedBrie.cs
@@ -0,0 +1,26 @@
+﻿namespace GildedRoseKata;
+
+public class AgedBrie : ItemWrapper
+{
+    public AgedBrie(Item item) : base(item)
+    {
+    }
+
+    public override void UpdateQuality()
+    {
+        if (_item.Quality < 50)
+        {
+            _item.Quality += 1;
+        }
+
+        _item.SellIn -= 1;
+
+        if (_item.SellIn < 0)
+        {
+            if (_item.Quality < 50)
+            {
+                _item.Quality += 1;
+            }
+        }
+    }
+}
